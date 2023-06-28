7f4e0e4d5ba42887f4805c8866703bddc0f45559
Xau <frozzy.xfs@gmail.com>
2023-06-27 20:12:46 -0400

    Add the four basic operations: add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,5 @@
+// Four basic operations
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
