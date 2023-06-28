d106112630431d9203273df7a74c701a465f877c
Xau <frozzy.xfs@gmail.com>
2023-06-27 20:18:57 -0400

    Add operate function

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let add = (num1, num2) => num1 + num2;
 let subtract = (num1, num2) => num1 - num2;
 let multiply = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
+
+// Operation variables
+let aNum = 0;
+let bNum = 0;
+let operator = "";
+
+let operate = (op, num1, num2) => {
+    if (op === "+") return add(num1, num2);
+    if (op === "-") return subtract(num1, num2);
+    if (op === "*") return multiply(num1, num2);
+    if (op === "/") return divide(num1, num2);
+}
